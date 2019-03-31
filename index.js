371c2a0bcfa32f067e48752a4c09b82762e01ff7
kaleido <kaleido@gmail.com>
2019-03-31 10:23:58 +0900

    implemente authentication

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express=require("express");
+const express = require("express");
+require("./services/passport")
 
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
-  res.send({bye:'buddy'})
-}
-)
+require("./routes/authRoutes")(app)
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
