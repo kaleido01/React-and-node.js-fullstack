0ccab512070d6fb234e4440877301cab77924e29
kaleido <kaleido@gmail.com>
2019-04-02 21:41:06 +0900

    finish test and get message

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,21 @@
+const keys=require("../../config/keys")
+
 module.exports= survey=>{
-  return '<div>' + survey.body +'</hi>'
+  return `
+          <html>
+            <body>
+              <div style="text-align:center;">
+                <h3>I'd like your input!</h3>
+                <p>${survey.body}</p>
+                <div>
+                  <a href="api/surveys/thanks">Yes</a>
+                </div>
+                <div>
+                  <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                </div>
+              <div>
+            </body>
+          </html>
+  
+  `
 }
