165c7b2a6f3afd1963d5668da93c94ec5849058a
kaleido <kaleido@gmail.com>
2019-04-02 13:31:51 +0900

    fix typo pass path

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ require("./routes/billingRoutes")(app);
 if(process.env.NODE_ENV==="production"){
   app.use(express.static("client/build"))
 
-  const path=require("pass")
+  const path=require("path")
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
   })
