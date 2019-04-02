9b0b725beedf9700d67956296656e78c04d99913
kaleido <kaleido@gmail.com>
2019-04-02 23:24:20 +0900

    implemente redux-form 次から実践的なフォーム作成

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,8 @@ import Header from "./Header";
 import { connect } from 'react-redux'
 import * as actions from '../actions' 
 import Landing from './Landing';
-const Surveys = () => <div> survey</div>;
+import Dashboard from "./Dashboard";
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component{
 
@@ -20,7 +21,8 @@ class App extends Component{
 
 				<Header />
 				<Route path="/" exact component={Landing} />
-				<Route path="/surveys" exact component={Surveys} />
+				<Route path="/surveys" exact component={Dashboard} />
+				<Route path="/surveys/new" exact component={SurveyNew} />
 			</div>
 		</BrowserRouter>
   );
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,19 @@
+import React,{ Component } from "react" 
+import SurveyForm from "./surveyForm";
+
+
+
+class SurveyNew extends Component{
+  render(){
+
+    return(
+      <div>
+        surveynew
+      <SurveyForm />
+      </div>
+    )
+
+  }
+}
+
+export default SurveyNew
