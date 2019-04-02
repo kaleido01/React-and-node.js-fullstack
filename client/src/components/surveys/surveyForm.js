826b3cf79dbfb16c1da1c2616a6ff5db445167a2
kaleido <kaleido@gmail.com>
2019-04-02 23:46:05 +0900

    4つのフォームを実装したが重複が多い

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -6,7 +6,12 @@ class SurveyForm extends Component{
   
   renderFields(){
     return (
-      <div><Field type="text" name="title" component={SurveyField}   /></div>
+      <div>
+        <Field label="SurveyTitle" type="text" name="title" component={SurveyField}   />
+        <Field label="Subject Line" type="text" name="subject" component={SurveyField}   />
+        <Field label="Email Body" type="text" name="body" component={SurveyField}   />
+        <Field label="Recipient List" type="emails" name="emails" component={SurveyField}   />
+        </div>
     )
   }
 
