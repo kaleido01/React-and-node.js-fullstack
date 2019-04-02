273da91030d9b20ea9938f2f81d881361dfd0081
kaleido <kaleido@gmail.com>
2019-04-03 00:38:14 +0900

    add form validate title

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -44,6 +44,16 @@ class SurveyForm extends Component{
   }
 }
 
+function validate(values){
+  const errors={}
+
+  if(!values.title){
+  errors.title="You must provide a title"
+}
+
+}
+
 export default reduxForm({
+  validate,
   form:"surveyForm"
 })(SurveyForm)
