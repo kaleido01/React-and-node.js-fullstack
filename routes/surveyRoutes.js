54a57ec5897272fa80bfe5969da49ba33aea9971
kaleido <kaleido@gmail.com>
2019-04-03 16:40:57 +0900

    わたってくるオブジェクトを適切な形に変換完了。次はデータベースに処理を送る

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,60 +1,57 @@
-const _ =require('lodash')
-const {Path}=require('path-parser')
-const{ URL }=require("url")
-const mongoose=require("mongoose")
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-const Mailer = require("../services/Mailer")
-const Survey=mongoose.model("surveys")
-const surveyTemplate=require("../services/emailTemplates/surveyTemplate")
+const Mailer = require("../services/Mailer");
+const Survey = mongoose.model("surveys");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = app => {
+	app.get("/api/surveys/thanks", (req, res) => {
+		res.send("Thank you for Voting!!!!");
+	});
 
-  app.get("/api/surveys/thanks",(req,res)=>{
-    res.send('Thank you for Voting!!!!')
-  })
+	app.post("/api/surveys/webhooks", (req, res) => {
+		const p = new Path("/api/surveys/:surveyId/:choice");
 
-  app.post("/api/surveys/webhooks",(req,res)=>{
-    const p=new Path('/api/surveys/:surveyId/:choice')
+		const events = _.chain(req.body)
+			.map(({ url, email }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return { email, surveyId: match.surveyId, choice: match.choice };
+				}
+			})
 
-
-    const events=_.map(req.body,(({url,email}) =>{
-      const match=p.test(new URL(url).pathname)
-      if(match){
-        return {email,surveyId:match.surveyId,choice:match.choice}
-      }
-    })
-    )
-    
-    const compactEvents=_.compact(events)
-    const uniqueEvents=_.uniqBy(compactEvents,"email","surveyId")
-    console.log(uniqueEvents) 
-   }
-   )
+			.compact()
+			.uniqBy("email", "surveyId")
+			.value();
+		console.log(events);
+	});
 
 	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    const { title, subject,body,recipients }=req.body
+		const { title, subject, body, recipients } = req.body;
 
-    const survey=new Survey({
-      title,
-      subject,
-      body,
-      recipients:recipients.split(",").map(email=>({email:email.trim()})),
-      _user:req.user.id,
-      dateSent:Date.now()
-    })
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: Date.now()
+		});
 
-    const mailer=new Mailer(survey,surveyTemplate(survey))
-    try{
-
-      await mailer.send()
-      await survey.save()
-      req.user.credits-=1
-      const user=await req.user.save()
-      res.send(user)
-    }catch(err){
-      res.status(422).send(err)
-      console.log(err)
-    }
-  });
+		const mailer = new Mailer(survey, surveyTemplate(survey));
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+			console.log(err);
+		}
+	});
 };
