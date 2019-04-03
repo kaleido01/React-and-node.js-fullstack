4940242d6be5e7a06edc3cdd181f459b8140d1f5
kaleido <kaleido@gmail.com>
2019-04-03 09:36:20 +0900

    finish redux form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default ({input, label})=>{
+export default ({input, label, meta:{error,touched}})=>{
   return (
     <div>
       <label>{label}</label>
       <input {...input}/>
+      <div className="red-text mb-4">
+      {touched && error }
+      </div>
     </div>
 
   )
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,18 +1,37 @@
-import React,{ Component } from "react" 
-import SurveyForm from "./surveyForm";
+// SurveyNew shows SurveyForm and SurveyFormReview
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
+class SurveyNew extends Component {
+  state = { showFormReview: false };
 
+  renderContent() {
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
+    }
 
-class SurveyNew extends Component{
-  render(){
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
 
-    return(
-      <div className="mt-5">
-      <SurveyForm />
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
       </div>
-    )
-
+    );
   }
 }
 
-export default SurveyNew
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
